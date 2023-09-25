66331f0a9ac50ec7830d13ce42a83b33e26502fd
AimarMurua <murua.aimar@uni.eus>
2023-09-25 12:33:44 +0200

    django proiektu berria hasita

---
+++ BideojokuDenda/BideojokuAlokairua/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+
+class Bideojokua(models.Model):
+    bid_id = models.CharField(max_length=50)
+    bid_izena = models.CharField(max_length=100)
+    bid_generoa = models.CharField(max_length=100)
+    bid_hizkuntza = models.CharField(max_length=50)
+    bid_irteeradata = models.DateField()
+    bid_adinminimoa = models.IntegerField()
+    bid_sinopsia = models.CharField(max_length=250)
+
+    def __unicode__(self):
+        return self.bid_izena
+    
+class Bezeroa(models.Model):
+    bez_dni = models.CharField(max_length=20)
+    bez_izena = models.CharField(max_length=30)
+    bez_abizena = models.CharField(max_length=50)
+    bez_telefonoa = models.CharField(max_length=20)
+    bez_email = models.CharField(max_length=50)
+    bez_helbidea = models.CharField(max_length=60)
+    
+    def __unicode__(self):
+        return self.bez_izena + " " + self.bez_abizena
