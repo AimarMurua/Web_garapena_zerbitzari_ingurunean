741ac68582cd98fa963949a7b151ebbe092ade03
AimarMurua <murua.aimar@uni.eus>
2023-09-28 14:27:33 +0200

    Django proiektua

--- BideojokuDenda/BideojokuAlokairua/urls.py
+++ BideojokuDenda/BideojokuAlokairua/urls.py
@@ -3,6 +3,17 @@ from .import views
 
 urlpatterns = [
     path('', views.index, name = 'index'),
+
     path('add/', views.add, name = 'add'),
-    path('add/addbid/', views.addbid, name = 'addbid')
+    path('add/addbid/', views.addbid, name = 'addbid'), #form action izena form action="addbid/"
+
+    path('add/addbez/', views.addbez, name= 'addbez'),
+    path('add/addbezero/', views.addbezero, name= 'addbezero'),
+    path('bezeroak/', views.bezeroak, name = 'bezeroak'),
+
+    path('ezabatubideojokua/<int:id>', views.ezabatubid, name = 'ezabatubid'),
+    path('bezeroak/ezabatubezeroa/<int:id>', views.ezabatubez, name = 'ezabatubez'),
+
+    path('eguneratubidform/<int:id>', views.eguneratubidform, name = 'eguneratubidform'),
+    path('eguneratubid/<int:id>', views.eguneratubid, name = 'eguneratubid'),
 ]   
--- BideojokuDenda/BideojokuAlokairua/views.py
+++ BideojokuDenda/BideojokuAlokairua/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Bideojokua
+from .models import Bezeroa
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
@@ -9,8 +10,15 @@ def index(request):
     jokuak = Bideojokua.objects.all
     return render(request, 'index.html', {'bideojokua':jokuak}) #ezkerrekoa 'bideojokua' htmlra pasatzen den aldagaiaren izena izango da. 
 
+def bezeroak(request):
+    bezeroak = Bezeroa.objects.all
+    return render(request, 'bezeroak.html', {'bezeroa':bezeroak})
+
 def add(request):
     return render(request, 'addbid.html')
+
+def addbez(request):
+    return render(request, 'addbez.html')
 
 def addbid(request):
     bid_id = request.POST["bid_id"]
@@ -31,4 +39,58 @@ def addbid(request):
                     bid_sinopsia = bid_sinopsia
                 )
     bid_berria.save()
+    return HttpResponseRedirect(reverse('index'))
+
+def addbezero(request):
+    bez_dni = request.POST["bez_dni"]
+    bez_izena = request.POST["bez_izena"]
+    bez_abizena = request.POST["bez_abizena"]
+    bez_telefonoa = request.POST["bez_telefonoa"]
+    bez_email = request.POST["bez_email"]
+    bez_helbidea = request.POST["bez_helbidea"]
+
+    bez_berria = Bezeroa(
+                    bez_dni = bez_dni,
+                    bez_izena = bez_izena,
+                    bez_abizena = bez_abizena,
+                    bez_telefonoa = bez_telefonoa, 
+                    bez_email = bez_email, 
+                    bez_helbidea = bez_helbidea
+                )
+    bez_berria.save()
+    return HttpResponseRedirect(reverse('bezeroak'))
+
+def ezabatubid(request, id):
+    ezabatzekobideojokua = Bideojokua.objects.get(id = id)
+    ezabatzekobideojokua.delete()
+    return HttpResponseRedirect(reverse('index'))
+
+def ezabatubez(request, id):
+    ezabatzekobezeroa = Bezeroa.objects.get(id = id)
+    ezabatzekobezeroa.delete()
+    return HttpResponseRedirect(reverse('bezeroak'))
+
+def eguneratubidform(request, id): 
+    eguneratzekobideojokua = Bideojokua.objects.get(id = id)
+    return render(request, 'eguneratubidform.html', {'bideojokua':eguneratzekobideojokua})
+
+def eguneratubid(request, id): 
+    eguneratzekobid = Bideojokua.objects.get(id = id)
+    bid_id = request.POST["bid_id"]
+    bid_izena = request.POST["bid_izena"]
+    bid_generoa = request.POST["bid_generoa"]
+    bid_hizkuntza = request.POST["bid_hizkuntza"]
+    bid_irteeradata = request.POST["bid_irteeradata"]
+    bid_adinminimoa = request.POST["bid_adinminimoa"]
+    bid_sinopsia = request.POST["bid_sinopsia"]
+
+    eguneratzekobid.bid_id = bid_id
+    eguneratzekobid.bid_izena = bid_izena
+    eguneratzekobid.bid_generoa = bid_generoa
+    eguneratzekobid.bid_hizkuntza = bid_hizkuntza
+    eguneratzekobid.bid_irteeradata = bid_irteeradata
+    eguneratzekobid.bid_adinminimoa = bid_adinminimoa
+    eguneratzekobid.bid_sinopsia = bid_sinopsia
+    eguneratzekobid.save()
+
     return HttpResponseRedirect(reverse('index'))
