394db8d5068b9feede432dddaac59b4623b874a3
Aimar Murua Olalde <murua.aimar@uni.eus>
2023-10-17 00:28:03 +0200

    jatetxea proiektua amaituta

--- jatetxea/AMOjatetxea/MahaiAlokairua/urls.py
+++ jatetxea/AMOjatetxea/MahaiAlokairua/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('gehitu/', views.gehitu, name = 'gehitu'),
     path('gehitu/gehitubezeroa/', views.gehitubezeroa, name = 'gehitubezeroa'),
     path('ezabatubezeroa/<dni>', views.ezabatubezeroa, name = 'ezabatubezeroa'),
+    path('eguneratubezeroa/<dni>', views.eguneratubezeroa, name = 'eguneratu'),
+    path('eguneratubezeroa/eguneratubezeroaerregistroa/<dni>', views.eguneratubezeroaerregistroa, name='eguneratubezeroaerregistroa'), 
+    path('alokairua/', views.alokairua, name = 'alokairua'),
+    path('alokairua/gehitualokairua/', views.gehitualokairua, name = 'gehitualokairua'),
+    path('alokairua/gehitualokairua/gehitualokairuaerregistroa/', views.gehitualokairuaerregistroa, name = 'gehitualokairuaerregistroa'),
 ]
--- jatetxea/AMOjatetxea/MahaiAlokairua/views.py
+++ jatetxea/AMOjatetxea/MahaiAlokairua/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Mahaia
 from .models import Bezeroa
+from .models import Alokairua
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
@@ -25,4 +26,45 @@ def gehitubezeroa(request):
 def ezabatubezeroa(request, dni):
     bezeroa = Bezeroa.objects.get(dni = dni)
     bezeroa.delete()
+    return HttpResponseRedirect(reverse('index'))
+
+def eguneratubezeroa(request, dni):
+    bezeroa = Bezeroa.objects.get(dni = dni)
+    return render(request, 'eguneratubezeroa.html', {'bezeroa':bezeroa})
+
+def eguneratubezeroaerregistroa(request, dni): 
+    dni = request.POST['dni']
+    izena = request.POST['izena']
+    abizena = request.POST['abizena']
+
+    bezeroa = Bezeroa.objects.get(dni = dni)
+    bezeroa.dni = dni
+    bezeroa.izena = izena
+    bezeroa.abizena = abizena
+
+    bezeroa.save()
+    return HttpResponseRedirect(reverse('index'))
+
+def alokairua(request):
+    alokairuak = Alokairua.objects.all
+    return render(request, 'alokairuak.html', {'alokairua':alokairuak})
+
+def gehitualokairua(request):
+    bezeroa = Bezeroa.objects.all
+    mahaia = Mahaia.objects.all
+
+    return render(request, 'gehitualokairua.html', {'bezeroa':bezeroa, 'mahaia':mahaia})
+
+def gehitualokairuaerregistroa(request): 
+    mahaia = request.POST['mahai_zenbakia']
+    mahaiaOndo = Mahaia.objects.get(zenbakia = mahaia)
+
+    bezeroa = request.POST['bezero_dni']
+    bezeroaOndo = Bezeroa.objects.get(izena = bezeroa)
+
+    alokairuData = request.POST['alokairu_data']
+    alokairuOrdua = request.POST['alokairu_ordua']
+
+    alokairua = Alokairua(mahai_zenbakia = mahaiaOndo, bezero_dni = bezeroaOndo, alokairu_data = alokairuData, alokairu_ordua = alokairuOrdua)
+    alokairua.save()
     return HttpResponseRedirect(reverse('index'))
