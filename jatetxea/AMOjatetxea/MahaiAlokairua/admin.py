6b0a2977cf84e803e4f2bf9aa52fce74d33a6e8f
Aimar Murua Olalde <murua.aimar@uni.eus>
2024-02-13 21:39:55 +0100

    .

--- jatetxea/AMOjatetxea/MahaiAlokairua/admin.py
+++ jatetxea/AMOjatetxea/MahaiAlokairua/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Mahaia
 from .models import Bezeroa
+from .models import Alokairua
 
 # Register your models here.
 
 admin.site.register(Mahaia)
 admin.site.register(Bezeroa)
+admin.site.register(Alokairua)
